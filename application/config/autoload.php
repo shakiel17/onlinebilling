8405913a4e40bf90319d0eedb7858697dc3a8907
shakiel17 <hisoka.aboy@gmail.com>
2025-06-10 08:49:34 +0800

    themes

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Sabong_model');
+$autoload['model'] = array('Billing_model');
--- application/config/routes.php
+++ application/config/routes.php
@@ -50,40 +50,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 //========================Admin Route==============================
-$route['change_password'] = 'pages/change_password';
-$route['user_list'] = 'pages/user_list';
-$route['delete_user_account/(:any)'] = 'pages/delete_user_account/$1';
-$route['save_user_account'] = 'pages/save_user_account';
-$route['user_account'] = 'pages/user_account';
-$route['print_income'] = 'pages/print_income';
-$route['income'] = 'pages/income';
-$route['fight_result/(:any)/(:any)'] = 'pages/fight_result/$1/$2';
-$route['close_betting/(:any)'] = 'pages/close_betting/$1';
-$route['active_fight'] = 'pages/active_fight';
-$route['create_fight'] = 'pages/create_fight';
-$route['fight_list'] = 'pages/fight_list';
-$route['cancel_withdrawal/(:any)'] = 'pages/cancel_withdrawal/$1';
-$route['approve_withdrawal/(:any)'] = 'pages/approve_withdrawal/$1';
-$route['manage_withdrawal'] = 'pages/manage_withdrawal';
-$route['cancel_deposit/(:any)'] = 'pages/cancel_deposit/$1';
-$route['approve_deposit/(:any)'] = 'pages/approve_deposit/$1';
-$route['manage_deposit'] = 'pages/manage_deposit';
 $route['admin_logout'] = 'pages/admin_logout';
 $route['adminmain'] = 'pages/adminmain';
 $route['admin_authenticate'] = 'pages/admin_authenticate';
 $route['admin'] = 'pages/admin';
-//=========================User Route============================
-$route['change_user_password'] = 'pages/change_user_password';
-$route['undo_bet/(:any)/(:any)/(:any)'] = 'pages/undo_bet/$1/$2/$3';
-$route['search_bet_history'] = 'pages/search_bet_history';
-$route['bet_history'] = 'pages/bet_history';
-$route['submit_bet'] = 'pages/submit_bet';
-$route['withdraw_history'] = 'pages/withdraw_history';
-$route['save_withdraw'] = 'pages/save_withdraw';
-$route['withdraw'] = 'pages/withdraw';
-$route['deposit_history'] = 'pages/deposit_history';
-$route['save_deposit'] = 'pages/save_deposit';
-$route['deposit'] = 'pages/deposit';
+//=========================School Route============================
 $route['main'] = 'pages/main';
 $route['logout'] = 'pages/logout';
 $route['registration'] = 'pages/registration';
